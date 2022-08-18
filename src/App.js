3fb71415f1eb6730ef28614f4ad8096dfd6cd171
Nada Gebsaap <nada.gebsaap@gmail.com>
2022-08-18 14:53:10 +0700

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CharacterCard from './CharacterCard';
-function App() {
- return (
- <div>
- <CharacterCard value="h"/>
- <CharacterCard value="i"/>
- </div>
- );
-}
+    const word = "Hello";
+    function App() {
+     return (
+     <div>
+     {
+     Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+     }
+     </div>
+     );
+    }
 export default App;
