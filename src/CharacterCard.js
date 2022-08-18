f29ad6c0550cdf2a4c051f627a0b261a2e7d2dfe
Nada Gebsaap <nada.gebsaap@gmail.com>
2022-08-18 14:57:08 +0700

    Styling

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,6 +2,6 @@ import React from 'react';
 import './App.css';
 export default function CharacterCard(props) {
  return (
- <div>{props.value}</div>
+ <div className='card'>{props.value}</div>
  )
 }
