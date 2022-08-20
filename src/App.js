242e8bcf831f37e77b0a9733f12ffe1cf7c1a950
Nada Gebsaap <nada.gebsaap@gmail.com>
2022-08-20 09:59:25 +0700

    Part7.2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import CharacterCard from './CharacterCard';
 import './App.css';
 import WordCard from './WordCard';
     const word = "Hello";
-    function App() {
-     return (
-     <div>
+
+function App() {
+    return (
+    <div className='App.header'>
+        
+        <p ></p>
+        Welcome to CARD GAME
+        <p></p>
      {
       <WordCard value="hello"/>
      }
      </div>
      );
     }
+
 export default App;
